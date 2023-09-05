86f9b37d417098e7720f8c798456c6cda5eda92b
AnatakuAmeerah <anatakuameerah@gmail.com>
2023-09-05 04:18:50 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Grid from './Grid';
+// import "./App.css";
+ import "./index.css";
+
+// solve the css, range utility and Grid component 
+
+function App() {
+  return (
+    <Grid
+      numRows={8}
+      numCols={6}
+    />
+  );
+}
+
+export default App;
---
+++ src/Grid.jsx
@@ -0,0 +1,16 @@
+import { range } from './utils';
+
+function Grid({ numRows, numCols }) {
+  const row = range(numRows).map((rows) => (
+    <div key={rows} className="row">
+      {range(numCols).map((col) => (
+        <div key={col} className="cell" />
+      ))}
+    </div>
+  ));
+
+
+  return <div className="grid">{row}</div>;
+}
+
+export default Grid;
